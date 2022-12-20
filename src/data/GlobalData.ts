e32084a01beaadf9fbe3dde79a30f8ccd93415eb
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-12-20 16:22:05 -0300

    refactor(data): change GlobalData to constants

--- src/data/GlobalData.ts
+++
@@ -1,6 +0,0 @@
-export const GLOBAL_DATA = {
-  gravity: 0.07,
-  groundY: 200,
-  worldXStart: 10,
-  worldXEnd: 500,
-};
---
+++ src/data/constants.ts
@@ -0,0 +1,5 @@
+export const GRAVITY = 0.8;
+export const FRICTION_MULTIPLIER = 0.1;
+export const GROUND_Y = 150;
+export const WORLD_X_START = 0;
+export const WORLD_X_END = 500;
