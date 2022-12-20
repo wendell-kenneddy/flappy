01e113721ea9150415042b3750b34c4f8222b0bd
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-12-20 16:27:27 -0300

    refactor(main): update boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import './styles/main.css';
 
-import { Rect } from './lib/Rect';
-import { GlobalRenderer } from './lib/GlobalRenderer';
-import { WindowInputSystem } from './lib/WindowInputSystem';
-import { Engine } from './lib/Engine';
+import { GlobalRenderer } from './core/renderers/GlobalRenderer';
+import { WindowInputSystem } from './core/input-systems/WindowInputSystem';
+import { Engine } from './core/Engine';
+import { Player } from './lib/Player';
+import { Rect } from './lib/primitives/Rect';
+import { GROUND_Y } from './data/constants';
 
 const canvas = document.getElementById('scene') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
@@ -12,56 +14,34 @@ const windowInputSystem = new WindowInputSystem();
 const globalRenderer = new GlobalRenderer(ctx);
 const engine = new Engine(windowInputSystem, globalRenderer);
 
-const p1 = new Rect(
-  {
-    x: canvas.width / 2,
-    y: 10,
-    dx: 1.5,
-    dy: 4,
-    width: 10,
-    height: 10,
-    color: '#34dbeb',
-  },
-  true
-);
-const p2 = new Rect(
-  {
-    x: canvas.width / 2 + 20,
-    y: 10,
-    dx: 3,
-    dy: 1,
-    width: 10,
-    height: 10,
-  },
-  true
-);
-const p3 = new Rect(
-  {
-    x: canvas.width / 2 + 40,
-    y: 10,
-    dx: 2,
-    dy: 4,
-    width: 10,
-    height: 10,
-    color: '#f542da',
-  },
-  true
-);
-const p4 = new Rect(
-  {
-    x: canvas.width / 2 + 60,
-    y: 10,
-    dx: 5,
-    dy: 1,
-    width: 10,
-    height: 10,
-    color: '#ff0000',
-  },
-  true
-);
+const ground = new Rect({
+  x: 0,
+  y: GROUND_Y,
+  dy: 0,
+  dx: 0,
+  width: canvas.width,
+  height: canvas.height - GROUND_Y,
+  supportsInputs: false,
+  color: '#ff0000',
+});
+const p1 = new Player({
+  x: 10,
+  y: 0,
+  dx: 3,
+  dy: 1,
+  width: 10,
+  height: 10,
+  supportsInputs: true,
+});
+const p2 = new Player({
+  x: 40,
+  y: 0,
+  dx: 4,
+  dy: 3,
+  width: 10,
+  height: 10,
+  supportsInputs: true,
+});
 
-engine.AddEntity(p1);
-engine.AddEntity(p2);
-engine.AddEntity(p3);
-engine.AddEntity(p4);
+engine.AddEntities([ground, p1, p2]);
 engine.Run();
