13903865ad37484af04b63e392aba7e47cb97f58
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-12-27 07:47:17 -0300

    refactor: move GameObject to engine directory

---
+++ src/engine/interfaces/GameObject.ts
@@ -0,0 +1,5 @@
+export abstract class GameObject {
+  abstract readonly id: string;
+  abstract Draw(ctx: CanvasRenderingContext2D): void;
+  abstract Update(state: any): void;
+}
--- src/interfaces/GameObject.ts
+++
@@ -1,9 +0,0 @@
-import { GameInput } from './Input';
-
-export abstract class GameObject {
-  abstract readonly id: string;
-  abstract readonly supportsInputs: boolean;
-  abstract Draw(ctx: CanvasRenderingContext2D): void;
-  abstract Update(): void;
-  abstract OnInput(i: GameInput): void;
-}
