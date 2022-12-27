fd3d912efe5981e1dece3346bec2114a24f25c15
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-12-27 08:05:48 -0300

    refactor(main.ts): update boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,60 @@
 import './styles/main.css';
 
-import { GlobalRenderer } from './core/renderers/GlobalRenderer';
-import { WindowInputSystem } from './core/input-systems/WindowInputSystem';
-import { Engine } from './core/Engine';
-import { Player } from './lib/Player';
+import { GlobalRenderer } from './engine/renderers/GlobalRenderer';
+import { KeyboardInputSystem } from './engine/input-systems/KeyboardInputSystem';
+import { CoreEngine } from './engine';
 import { Rect } from './lib/primitives/Rect';
-import { GROUND_Y } from './data/constants';
+import { GROUND_Y, WORLD_X_START } from './data/constants';
+import { Bird } from './lib/Bird';
+import { PipeSpawnerScript } from './lib/scripts/PipeSpawner';
 
 const canvas = document.getElementById('scene') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-const windowInputSystem = new WindowInputSystem();
+const keyboardInputSystem = new KeyboardInputSystem();
 const globalRenderer = new GlobalRenderer(ctx);
-const engine = new Engine(windowInputSystem, globalRenderer);
+const engine = new CoreEngine(keyboardInputSystem, globalRenderer);
 
+const skyBox = new Rect({
+  x: 0,
+  y: 0,
+  width: canvas.width,
+  height: canvas.height,
+  color: '#b0fbff',
+});
 const ground = new Rect({
   x: 0,
   y: GROUND_Y,
-  dy: 0,
-  dx: 0,
   width: canvas.width,
   height: canvas.height - GROUND_Y,
-  supportsInputs: false,
-  color: '#ff0000',
+  color: '#2b1b0f',
 });
-const p1 = new Player({
-  x: 10,
-  y: 0,
-  dx: 3,
+const groundSurface = new Rect({
+  x: 0,
+  y: GROUND_Y,
+  width: canvas.width,
+  height: 5,
+  color: '#379914',
+});
+const p1 = new Bird({
+  x: WORLD_X_START + 50,
+  y: 10,
   dy: 1,
   width: 10,
   height: 10,
-  supportsInputs: true,
+  color: '#ff0000',
 });
-const p2 = new Player({
-  x: 40,
-  y: 0,
-  dx: 4,
-  dy: 3,
-  width: 10,
-  height: 10,
-  supportsInputs: true,
-});
-
-engine.AddEntities([ground, p1, p2]);
+engine.AddGameObjects([skyBox, ground, groundSurface, p1]);
+engine.AttachScript(
+  new PipeSpawnerScript({
+    spawnInterval: 60,
+    pipePairConfig: {
+      x: 330,
+      y: 0,
+      dx: 2,
+      gap: 70,
+      color: '#0000ff',
+    },
+  })
+);
 engine.Run();
