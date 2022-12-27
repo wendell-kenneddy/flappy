c689297e6e3ec4c9a6ec6536426b650091e9637c
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-12-27 07:51:29 -0300

    refactor(constants): update values

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,5 +1,4 @@
-export const GRAVITY = 0.8;
-export const FRICTION_MULTIPLIER = 0.1;
-export const GROUND_Y = 150;
+export const GRAVITY = 0.1;
+export const FRICTION_MULTIPLIER = 0.5;
+export const GROUND_Y = 230;
 export const WORLD_X_START = 0;
-export const WORLD_X_END = 500;
