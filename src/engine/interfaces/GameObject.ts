914109ad2a8c3706574de9d08568f665ad47ee17
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-12-30 07:49:07 -0300

    feat(engine_interfaces): add id to scripts abstract class

--- src/engine/interfaces/GameObject.ts
+++ src/engine/interfaces/GameObject.ts
@@ -2,4 +2,5 @@ export abstract class GameObject {
   abstract readonly id: string;
   abstract Draw(ctx: CanvasRenderingContext2D): void;
   abstract Update(state: any): void;
+  abstract GetState(): any;
 }
--- src/engine/interfaces/Scripts.ts
+++ src/engine/interfaces/Scripts.ts
@@ -1,6 +1,7 @@
 import { EngineState } from './Engine';
 
 export abstract class LogicScript {
+  abstract readonly id: string;
   abstract Tick(s: EngineState): void;
   abstract Cancel(): void;
 }
